50b39fcee5638798fead6ba82bb18145b05f8cfa
quadradoid <gabrielnogueiranogueira8@gmail.com>
2023-09-07 19:50:07 -0300

    terminando detalhes

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,122 +1,154 @@
-import { catalogo, salvarLocalStorage, lerLocalStorage} from "./utilidades";
+import { catalogo, salvarLocalStorage, lerLocalStorage } from "./utilidades";
 
-export const idsProdutoCarrinhoQuandtidade = lerLocalStorage('carrinho') ?? {}
-
+export const idsProdutoCarrinhoQuandtidade = lerLocalStorage("carrinho") ?? {};
 
 function abrirCarrinho() {
-    document.getElementById('carrinho').classList.remove('right-[-350px]');
-    document.getElementById('carrinho').classList.add('right-[0px]');
-};
+  document.getElementById("carrinho").classList.remove("right-[-350px]");
+  document.getElementById("carrinho").classList.add("right-[0px]");
+}
 
 function fecharCarrinho() {
-    document.getElementById('carrinho').classList.remove('right-[0px]');
-    document.getElementById('carrinho').classList.add('right-[-350px]');
-
-};
+  document.getElementById("carrinho").classList.remove("right-[0px]");
+  document.getElementById("carrinho").classList.add("right-[-350px]");
+}
 
 export function inicializarCarrinho() {
-    const botaoFechar = document.getElementById('fecharCarrinho');
-    const botaoAbrir = document.getElementById('abrirCarrinho');
-    const botaoFinalizarCompra = document.getElementById('finalizar-compra')
+  const botaoFechar = document.getElementById("fecharCarrinho");
+  const botaoAbrir = document.getElementById("abrirCarrinho");
+  const botaoFinalizarCompra = document.getElementById("finalizar-compra");
 
-    botaoFinalizarCompra.addEventListener('click',irParaCheckout)
+  botaoFinalizarCompra.addEventListener("click", irParaCheckout);
 
-    botaoFechar.addEventListener('click', fecharCarrinho);
-    botaoAbrir.addEventListener('click', abrirCarrinho);
-};
-
-function removerDoCarrinho(idProduto){
-    delete idsProdutoCarrinhoQuandtidade[idProduto];
-    salvarLocalStorage('carrinho',idsProdutoCarrinhoQuandtidade);
-    atualizarPrecoCarrinho();
-    renderizarProdutosCarrinho();
+  botaoFechar.addEventListener("click", fecharCarrinho);
+  botaoAbrir.addEventListener("click", abrirCarrinho);
 }
 
-function incrementarQuantidadeProduto(idProduto){
-    idsProdutoCarrinhoQuandtidade[idProduto]++;
-    salvarLocalStorage('carrinho',idsProdutoCarrinhoQuandtidade);
-    atualizarPrecoCarrinho();
-    atualizarInformacaoProduto(idProduto);
+function removerDoCarrinho(idProduto) {
+  delete idsProdutoCarrinhoQuandtidade[idProduto];
+  salvarLocalStorage("carrinho", idsProdutoCarrinhoQuandtidade);
+  atualizarPrecoCarrinho();
+  renderizarProdutosCarrinho();
 }
 
-function decrementarQuantidadeProduto(idProduto){
-    if(idsProdutoCarrinhoQuandtidade[idProduto]===1){
-        removerDoCarrinho(idProduto);
-        return;
-    }
-    idsProdutoCarrinhoQuandtidade[idProduto]--;
-    salvarLocalStorage('carrinho',idsProdutoCarrinhoQuandtidade);
-    atualizarPrecoCarrinho();
-    atualizarInformacaoProduto(idProduto);
-}
-function atualizarInformacaoProduto(idProduto){
-    const produto = catalogo.find((p) => p.id === idProduto);
-    const precoBr = produto.preco*idsProdutoCarrinhoQuandtidade[idProduto]
-    document.getElementById(`quantidade-${idProduto}`).innerText=idsProdutoCarrinhoQuandtidade[idProduto];
-    document.getElementById(`preco-${idProduto}`).innerText=`${precoBr.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})}`
+function incrementarQuantidadeProduto(idProduto) {
+  idsProdutoCarrinhoQuandtidade[idProduto]++;
+  salvarLocalStorage("carrinho", idsProdutoCarrinhoQuandtidade);
+  atualizarPrecoCarrinho();
+  atualizarInformacaoProduto(idProduto);
 }
 
-export function desenharProdutoNoCarrinho(idProduto){
-    const produto = catalogo.find((p) => p.id === idProduto);
-    const containerProdutosCarrinhos = document.getElementById('produtos-carrinho');
-    const elementoArticle = document.createElement('article');
-    const articleClasses=['flex', 'items-center', 'bg-teal-100', 'rounded-lg', 'p-1', 'relative']
-    for(const articleClass of articleClasses){
-        elementoArticle.classList.add(articleClass)
-    }
-    const precoBr=produto.preco*idsProdutoCarrinhoQuandtidade[idProduto]
-    const cardPodutoCarrinho = `<button id="remover-item-${produto.id}" class="absolute top-0 right-1"><i
+function decrementarQuantidadeProduto(idProduto) {
+  if (idsProdutoCarrinhoQuandtidade[idProduto] === 1) {
+    removerDoCarrinho(idProduto);
+    return;
+  }
+  idsProdutoCarrinhoQuandtidade[idProduto]--;
+  salvarLocalStorage("carrinho", idsProdutoCarrinhoQuandtidade);
+  atualizarPrecoCarrinho();
+  atualizarInformacaoProduto(idProduto);
+}
+function atualizarInformacaoProduto(idProduto) {
+  const produto = catalogo.find((p) => p.id === idProduto);
+  const precoBr = produto.preco * idsProdutoCarrinhoQuandtidade[idProduto];
+  document.getElementById(`quantidade-${idProduto}`).innerText =
+    idsProdutoCarrinhoQuandtidade[idProduto];
+  document.getElementById(
+    `preco-${idProduto}`
+  ).innerText = `${precoBr.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })}`;
+}
+
+export function desenharProdutoNoCarrinho(idProduto) {
+  const produto = catalogo.find((p) => p.id === idProduto);
+  const containerProdutosCarrinhos =
+    document.getElementById("produtos-carrinho");
+  const elementoArticle = document.createElement("article");
+  const articleClasses = [
+    "flex",
+    "items-center",
+    "bg-teal-100",
+    "rounded-lg",
+    "p-1",
+    "relative",
+  ];
+  for (const articleClass of articleClasses) {
+    elementoArticle.classList.add(articleClass);
+  }
+  const precoBr = produto.preco * idsProdutoCarrinhoQuandtidade[idProduto];
+  const cardPodutoCarrinho = `<button id="remover-item-${
+    produto.id
+  }" class="absolute top-0 right-1"><i
             class="fa-solid fa-circle-xmark text-teal-500 hover:text-teal-900"></i></button>
-        <img src="assets/${produto.nomeArquivoImagem}" class="h-14 rounded-lg" alt="">
+        <img src="assets/${
+          produto.nomeArquivoImagem
+        }" class="h-14 rounded-lg" alt="">
         <div class="p-2 flex flex-col justify-between">
             <p class="text-teal-900 text-sm">${produto.nome}</p>
-            <p class="text-green-700 text-lg" id="preco-${produto.id}">${precoBr.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})}</p>
+            <p class="text-green-700 text-lg" id="preco-${
+              produto.id
+            }">${precoBr.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })}</p>
         </div>
         <div class="flex gap-2 text-lg text-teal-950 items-end absolute right-2 bottom-0">
             <button id="decrementar-produto-${produto.id}">-</button>
-            <p id="quantidade-${produto.id}" >${idsProdutoCarrinhoQuandtidade[produto.id]}</p>
+            <p id="quantidade-${produto.id}" >${
+    idsProdutoCarrinhoQuandtidade[produto.id]
+  }</p>
             <button id="incrementar-produto-${produto.id}">+</button>
         </div>`;
-    
-    elementoArticle.innerHTML=cardPodutoCarrinho;
-    containerProdutosCarrinhos.appendChild(elementoArticle);
 
-    document.getElementById(`decrementar-produto-${produto.id}`).addEventListener('click',()=>decrementarQuantidadeProduto(produto.id))
-    document.getElementById(`incrementar-produto-${produto.id}`).addEventListener('click',()=>incrementarQuantidadeProduto(produto.id))
-    document.getElementById(`remover-item-${produto.id}`).addEventListener('click',()=>removerDoCarrinho(produto.id))
+  elementoArticle.innerHTML = cardPodutoCarrinho;
+  containerProdutosCarrinhos.appendChild(elementoArticle);
+
+  document
+    .getElementById(`decrementar-produto-${produto.id}`)
+    .addEventListener("click", () => decrementarQuantidadeProduto(produto.id));
+  document
+    .getElementById(`incrementar-produto-${produto.id}`)
+    .addEventListener("click", () => incrementarQuantidadeProduto(produto.id));
+  document
+    .getElementById(`remover-item-${produto.id}`)
+    .addEventListener("click", () => removerDoCarrinho(produto.id));
 }
 
-export function renderizarProdutosCarrinho(){
-    const containerProdutosCarrinhos = document.getElementById('produtos-carrinho');
-    containerProdutosCarrinhos.innerHTML='';
-    for(const idProduto in idsProdutoCarrinhoQuandtidade){
-        desenharProdutoNoCarrinho(idProduto);
-    }
+export function renderizarProdutosCarrinho() {
+  const containerProdutosCarrinhos =
+    document.getElementById("produtos-carrinho");
+  containerProdutosCarrinhos.innerHTML = "";
+  for (const idProduto in idsProdutoCarrinhoQuandtidade) {
+    desenharProdutoNoCarrinho(idProduto);
+  }
 }
 
 export function adiconarAoCarrinho(idProduto) {
-    if(idProduto in idsProdutoCarrinhoQuandtidade){
-        incrementarQuantidadeProduto(idProduto);
-        return
-    }
-    idsProdutoCarrinhoQuandtidade[idProduto] = 1;
-    salvarLocalStorage('carrinho',idsProdutoCarrinhoQuandtidade);
-    atualizarPrecoCarrinho();
-    desenharProdutoNoCarrinho(idProduto);
-};
-
-export function atualizarPrecoCarrinho(){
-    const precoCarrinho= document.getElementById('preco-total');
-    let precoTotalCarrinho=0;
-    for(const idProdutoNoCarrinho in idsProdutoCarrinhoQuandtidade){
-        precoTotalCarrinho+=catalogo.find(p=>p.id===idProdutoNoCarrinho).preco *idsProdutoCarrinhoQuandtidade[idProdutoNoCarrinho];
-    }
-    precoCarrinho.innerText=`Total:R$ ${precoTotalCarrinho}`;
+  if (idProduto in idsProdutoCarrinhoQuandtidade) {
+    incrementarQuantidadeProduto(idProduto);
+    return;
+  }
+  idsProdutoCarrinhoQuandtidade[idProduto] = 1;
+  salvarLocalStorage("carrinho", idsProdutoCarrinhoQuandtidade);
+  atualizarPrecoCarrinho();
+  desenharProdutoNoCarrinho(idProduto);
 }
 
-function irParaCheckout(){
-    if(Object.keys(idsProdutoCarrinhoQuandtidade).length===0){
-        return;
-    }
-    window.location.href=window.location.origin+"/checkout.html";
+export function atualizarPrecoCarrinho() {
+  const precoCarrinho = document.getElementById("preco-total");
+  let precoTotalCarrinho = 0;
+  for (const idProdutoNoCarrinho in idsProdutoCarrinhoQuandtidade) {
+    precoTotalCarrinho +=
+      catalogo.find((p) => p.id === idProdutoNoCarrinho).preco *
+      idsProdutoCarrinhoQuandtidade[idProdutoNoCarrinho];
+  }
+  precoCarrinho.innerText = `Total:R$ ${precoTotalCarrinho}`;
+}
+
+function irParaCheckout() {
+  if (Object.keys(idsProdutoCarrinhoQuandtidade).length === 0) {
+    return;
+  }
+  window.location.href = window.location.origin + "/checkout.html";
 }
--- src/utilidades.js
+++ src/utilidades.js
@@ -1,61 +1,77 @@
 export const catalogo = [
-    {
-        id: "1",
-        nome: 'Hamburgão Top',
-        preco: 25.50,
-        nomeArquivoImagem: 'produto1.png',
-        bebida:false
-    },
-    {
-        id: "2",
-        nome: 'Hamburgão Menos Top',
-        preco: 20.00,
-        nomeArquivoImagem: 'produto2.png',
-        bebida:false
-    },
-    {
-        id: "3",
-        nome: 'Batata Frita',
-        preco: 15.00,
-        nomeArquivoImagem: 'produto3.jpg',
-        bebida:true
-    }
+  {
+    id: "1",
+    nome: "Hamburgão Top",
+    preco: 25.5,
+    nomeArquivoImagem: "produto1.png",
+    bebida: false,
+  },
+  {
+    id: "2",
+    nome: "Hamburgão Menos Top",
+    preco: 20.0,
+    nomeArquivoImagem: "produto2.png",
+    bebida: false,
+  },
+  {
+    id: "3",
+    nome: "Batata Frita",
+    preco: 15.0,
+    nomeArquivoImagem: "produto3.jpg",
+    bebida: true,
+  },
 ];
 
-export function salvarLocalStorage(chave,informacao){
-    localStorage.setItem(chave,JSON.stringify(informacao))
+export function salvarLocalStorage(chave, informacao) {
+  localStorage.setItem(chave, JSON.stringify(informacao));
 }
 
-export function lerLocalStorage(chave){
-    return JSON.parse(localStorage.getItem(chave))
+export function lerLocalStorage(chave) {
+  return JSON.parse(localStorage.getItem(chave));
 }
 
-export function desenharProdutoNoCarrinhoSimples(idProduto,idContainerHTML,quantidadeProduto){
-    const produto = catalogo.find((p) => p.id === idProduto);
-    const containerProdutosCarrinhos = document.getElementById(idContainerHTML);
+export function desenharProdutoNoCarrinhoSimples(
+  idProduto,
+  idContainerHTML,
+  quantidadeProduto
+) {
+  const produto = catalogo.find((p) => p.id === idProduto);
+  const containerProdutosCarrinhos = document.getElementById(idContainerHTML);
 
-    const elementoArticle = document.createElement('article');
-    const articleClasses=['flex', 'bg-stone-200', 'rounded-lg', 'p-1', 'relative', 'm-2'];
-    for(const articleClass of articleClasses){
-        elementoArticle.classList.add(articleClass)
-    }
-    const precoBr=produto.preco*quantidadeProduto
-    const cardPodutoCarrinho = `
+  const elementoArticle = document.createElement("article");
+  const articleClasses = [
+    "flex",
+    "bg-stone-200",
+    "rounded-lg",
+    "p-1",
+    "relative",
+    "m-2",
+  ];
+  for (const articleClass of articleClasses) {
+    elementoArticle.classList.add(articleClass);
+  }
+  const precoBr = produto.preco * quantidadeProduto;
+  const cardPodutoCarrinho = `
         <img src="assets/${produto.nomeArquivoImagem}" 
         class="h-24 rounded-lg"
         alt="">
         <div class="p-2 flex flex-col justify-between">
             <p class="text-teal-900 text-sm">${produto.nome}</p>
-            <p class="text-green-700 text-lg">${precoBr.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})}</p>
+            <p class="text-green-700 text-lg">${precoBr.toLocaleString(
+              "pt-BR",
+              { style: "currency", currency: "BRL" }
+            )}</p>
         </div>
         <div class="flex text-lg text-teal-700 items-end absolute right-2 bottom-0">
-            <p id="quantidade-${produto.id}" class="ml-2" >${quantidadeProduto}</p>
+            <p id="quantidade-${
+              produto.id
+            }" class="ml-2" >${quantidadeProduto}</p>
         </div>`;
-    
-    elementoArticle.innerHTML=cardPodutoCarrinho;
-    containerProdutosCarrinhos.appendChild(elementoArticle);
+
+  elementoArticle.innerHTML = cardPodutoCarrinho;
+  containerProdutosCarrinhos.appendChild(elementoArticle);
 }
 
-export function apagarLocalStorage(chave){
-    localStorage.removeItem(chave)
+export function apagarLocalStorage(chave) {
+  localStorage.removeItem(chave);
 }
